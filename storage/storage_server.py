0d7ce2b8354ee4ca8e91e37fd59d2a621b3e6cd8
alexcogojocaru <alex.cojocaru73@gmail.com>
2022-06-02 00:56:21 +0300

    loguru for logging tracepoints

--- storage/storage_server.py
+++ storage/storage_server.py
@@ -1,18 +1,21 @@
+import sys
 import grpc
 import storage_pb2
 import storage_pb2_grpc
 
 from concurrent import futures
+from loguru import logger
 
 class StorageServicer(storage_pb2_grpc.StorageServicer):
     def __init__(self) -> None:
         super().__init__()
     
     def Store(self, request, context):
-        print(request)
+        logger.debug(f'traceid={request.traceID} spanid={request.spanID}')
         return storage_pb2.StorageResponse(status=200, message='buna')
 
 if __name__ == '__main__':
+    logger.debug('Starting server on port 50051')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     storage_pb2_grpc.add_StorageServicer_to_server(StorageServicer(), server)
 
